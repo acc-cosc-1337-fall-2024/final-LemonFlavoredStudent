f2d859f416ce115e1b7800ecf0fad1c8e73a636b
LemonFlavoredStudent <micah.vickery@g.austincc.edu>
2024-12-13 00:17:41 +0000

    Question 3 WIP

--- src/shooter.h
+++ src/shooter.h
@@ -1,1 +1,19 @@
-//
+#include "die.h"
+#include "roll.h"
+#include <vector>
+#ifndef SHOOTER_H
+#define SHOOTER_H
+
+class Shooter
+{
+    public:
+    Roll* throw_dice(Die& d1, Die& d2);
+    void display_rolled_values();
+    ~Shooter();
+
+    private:
+    std::vector<Roll*> rolls; //stores Roll pointers
+
+
+};
+#endif
